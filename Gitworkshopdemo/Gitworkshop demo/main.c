eb15e80b219fbdf39c8530220f254910faac847e
Jialin Zhang <1155141417@link.cuhk.edu.hk>
2021-03-04 15:07:41 +0800

    First Commit

---
+++ Gitworkshopdemo/Gitworkshop demo/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Gitworkshop demo
+//
+//  Created by zjl on 4/3/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
